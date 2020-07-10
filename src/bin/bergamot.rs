33ff9853b8aac22b17a0e02a282e4590c9e4f156
toshokan <toshokan@shojigate.net>
2020-07-09 21:20:47 -0400

    Create window

--- src/bin/bergamot.rs
+++ src/bin/bergamot.rs
@@ -13,5 +13,25 @@ fn main() -> Result<(), Error> {
     let (conn, _) = xcb::Connection::connect(None)?;
     let screen = conn.get_setup().roots().next()
         .expect("Failed to get screen");
+    let win = conn.generate_id();
+
+    let (width, height) = (1920, 18);
+
+    xcb::create_window(
+	&conn,
+	xcb::COPY_FROM_PARENT as u8,
+	win,
+	screen.root(),
+	0,
+	14,
+	width,
+	height,
+	0,
+	xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
+	screen.root_visual(),
+	&[
+	    (xcb::CW_BACK_PIXEL, screen.white_pixel()),
+	]
+    );
     Ok(())
 }
