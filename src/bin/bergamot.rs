31928b58a7f254d8354d17dcfb5016e8ded7d120
toshokan <toshokan@shojigate.net>
2020-07-13 18:03:05 -0400

    Display text in areas

--- src/bin/bergamot.rs
+++ src/bin/bergamot.rs
@@ -30,7 +30,7 @@ struct Area {
     tag: String,
     fg: Colour,
     bg: Option<Colour>,
-    onclick: Option<String>
+    onclick: Option<String>,
 }
 
 struct Paint {
@@ -122,6 +122,8 @@ fn main() -> Result<(), Error> {
     while let Some(event) = conn.wait_for_event() {
         match event.response_type() & !0x80 {
             xcb::EXPOSE => {
+                let font = pango::FontDescription::from_string("monospace 12");
+
                 let (width, height) = (width.into(), height.into());
                 ctx.set_source_rgb(0.1, 0.1, 0.1);
                 ctx.rectangle(0.0, 0.0, width, height);
@@ -131,35 +133,48 @@ fn main() -> Result<(), Error> {
                     Area {
                         align: Align::Left,
                         tag: "left".to_string(),
-                        text: "clicky".to_string(),
+                        text: "clicky_left".to_string(),
                         fg: Colour {
                             red: 1.0,
                             green: 0.0,
                             blue: 0.0,
                         },
-			bg: None,
-			onclick: None
+                        bg: None,
+                        onclick: None,
                     },
                     Area {
                         align: Align::Right,
                         tag: "left".to_string(),
-                        text: "clicky".to_string(),
+                        text: "clicky_right".to_string(),
                         fg: Colour {
                             red: 0.0,
                             green: 0.0,
                             blue: 1.0,
                         },
-			bg: None,
-			onclick: None
+                        bg: None,
+                        onclick: None,
                     },
                 ];
 
                 let mut left_finger = 0_f64;
                 let mut right_finger = width;
                 for area in areas.drain(..) {
-                    let area_width: f64 = 100_f64;
+                    let layout = pangocairo::create_layout(&ctx)
+                        .expect("Failed to create pangocairo layout");
+		    layout.set_font_description(Some(&font));
+		    layout.set_text(&area.text);
 
-                    ctx.set_source_rgb(area.fg.red, area.fg.green, area.fg.blue);
+		    let (w, h) = layout.get_pixel_size();
+		    let area_width: f64 = (w + 10).into();
+		    let layout_height: f64 = h.into();
+
+		    let bg = area.bg.as_ref().unwrap_or(&Colour {
+			red: 0_f64,
+			blue: 0_f64,
+			green: 0_f64
+		    });
+
+                    ctx.set_source_rgb(bg.red, bg.green, bg.blue);
                     let (left, right) = match area.align {
                         Align::Left => {
                             let (left, right) = (left_finger, left_finger + area_width);
@@ -173,7 +188,11 @@ fn main() -> Result<(), Error> {
                         }
                     };
                     ctx.rectangle(left, 0_f64, right - left, height);
-                    ctx.fill();
+		    ctx.fill();
+
+		    ctx.set_source_rgb(area.fg.red, area.fg.green, area.fg.blue);
+		    ctx.move_to(left + 5_f64, height/2_f64 - layout_height/2_f64);
+		    pangocairo::show_layout(&ctx, &layout);
 
                     area_paints.push(Paint { left, right, area })
                 }
