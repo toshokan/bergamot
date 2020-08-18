d2309d11fb481d60bbaec927a838d77be08b0fc8
toshokan <toshokan@shojigate.net>
2020-08-18 16:45:30 -0400

    Don't bring in cairo-sys just to name the type in cast

--- src/bin/bergamot.rs
+++ src/bin/bergamot.rs
@@ -104,8 +104,8 @@ fn main() -> Result<(), Error> {
         .expect("No visuals")
         .base;
 
-    let vp = &visp as *const xcb::ffi::xproto::xcb_visualtype_t as *mut cairo_sys::xcb_visualtype_t;
-    let cp = conn.get_raw_conn() as *mut cairo_sys::xcb_connection_t;
+    let vp = unsafe { std::mem::transmute(&visp) };
+    let cp = unsafe { std::mem::transmute(&conn.get_raw_conn()) };
     let cvis = unsafe { cairo::XCBVisualType::from_raw_borrow(vp) };
     let ccon = unsafe { cairo::XCBConnection::from_raw_borrow(cp) };
     let cwin = cairo::XCBDrawable(win);
